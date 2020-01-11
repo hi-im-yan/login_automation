6e612debceabd4c8bc9e9e951624efabf842d0c2
Hi-im-Yan <yanajiki2010@hotmail.com>
2020-01-11 19:23:38 -0400

    Otimizado em 5 segundos, não precisa mais usar sleep()

--- log_me_in.py
+++ log_me_in.py
@@ -10,85 +10,75 @@ with open("C:\Projetos\Python\Selenium\\login_automation\config.json") as config
 driver = webdriver.Chrome(executable_path=r"C:\WebDriver\chromedriver.exe")
 tab = 1
 
-# login Facebook
+# login Facebook & load next app
 try:
     driver.get("https://pt-br.facebook.com/login/")
+    driver.execute_script('''window.open("https://www.instagram.com/accounts/login/?hl=pt-br","_blank");''') 
     driver.find_element_by_name("email").send_keys(credential['facebook']['un'])
     driver.find_element_by_name("pass").send_keys(credential['facebook']['pw'])
     driver.find_element_by_name("pass").send_keys(Keys.ENTER)
     print("Facebook Logged")
-    time.sleep(1)
 except:
     print("Failed to log on Facebook")
 
-#login instagram
+#login instagram & load next app
 try:
-    driver.execute_script('''window.open("https://www.instagram.com/accounts/login/?hl=pt-br","_blank");''')
     driver.switch_to.window(driver.window_handles[tab])
-    time.sleep(1)
+    driver.execute_script('''window.open("https://web.whatsapp.com/","_blank");''')
     driver.find_element_by_tag_name("button").click()
-    time.sleep(1)
     print("Instagram Logged")
     tab += 1
 except:
     print("Failed to log on instagram")
 
-# #login whats
+# #login whats & load next app
 try:
-    driver.execute_script('''window.open("https://web.whatsapp.com/","_blank");''')
     driver.switch_to.window(driver.window_handles[tab])
-    time.sleep(1)
+    driver.execute_script('''window.open("https://github.com/login","_blank");''')
     print("WhatsApp Loaded")
     tab += 1
 except:
     print("Failed to log on whats")
 
-# #login github
+# #login github & load next app
 try:
-    driver.execute_script('''window.open("https://github.com/login","_blank");''')
     driver.switch_to.window(driver.window_handles[tab])
+    driver.execute_script('''window.open("https://discordapp.com/login","_blank");''')
     driver.find_element_by_name("login").send_keys(credential['github']['un'])
     driver.find_element_by_name("password").send_keys(credential['github']['pw'])
     driver.find_element_by_name("commit").click()
-    time.sleep(1)
     print("Github Logged")
     tab += 1
 except:
     print("Failed to log on github")
 
-
-
-#login discord
+#login discord & load next app
 try:
-    driver.execute_script('''window.open("https://discordapp.com/login","_blank");''')
     driver.switch_to.window(driver.window_handles[tab])
-    time.sleep(1)
+    driver.execute_script('''window.open("https://iqoption.com/en/login","_blank");''')
     driver.find_element_by_xpath("/html/body/div/div[1]/div/div[2]/div/form/div/div/div[1]/div[3]/div[1]/div/input").send_keys(credential['discord']['un'])
     driver.find_element_by_xpath("/html/body/div/div[1]/div/div[2]/div/form/div/div/div[1]/div[3]/div[2]/div/input").send_keys(credential['discord']['pw'])
     driver.find_element_by_xpath("/html/body/div/div[1]/div/div[2]/div/form/div/div/div[1]/div[3]/button[2]").click()
-    time.sleep(1)
     print("Discord Logged")
     tab += 1
 except:
     print("Failed to log na Discord")
 
-
+#login IQOption & load next app
 try:
-    driver.execute_script('''window.open("https://iqoption.com/en/login","_blank");''')
     driver.switch_to.window(driver.window_handles[tab])
-    time.sleep(2)
     driver.find_element_by_name("email").send_keys(credential['iqOption']['un'])
     driver.find_element_by_name("password").send_keys(credential['iqOption']['pw'])
     driver.find_element_by_xpath("/html/body/div[3]/div/div[1]/div/div/form/button[1]").click()
-    time.sleep(1)
     print("IQOption Logged")
     tab += 1
 except:
     print("Falha ao logar na iqOption")
+
 #template
 # try:
-#     driver.execute_script('''window.open("login_url","_blank");''')
 #     driver.switch_to.window(driver.window_handles[tab])
+#     driver.execute_script('''window.open("login_url","_blank");''')
 #     time.sleep(1)
 #     driver.find_element_by_name("login").send_keys(credential['aplication']['un'])
 #     driver.find_element_by_name("password").send_keys(credential['aplication']['pw'])
